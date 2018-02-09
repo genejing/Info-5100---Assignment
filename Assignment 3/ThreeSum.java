639de8e2a58f561e30058d13e10918c1a162341b
genejing <jingxinhao1992@gmail.com>
2018-02-09 14:44:07 -0800

    Second question

---
+++ Assignment 3/ThreeSum.java
@@ -0,0 +1,29 @@
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+public class ThreeSum {
+	public List<List<Integer>> threeSum(int[] nums) {
+		List<List<Integer>> res = new ArrayList<>();
+		Arrays.sort(nums);
+		for (int i = 0; i < nums.length - 2; i++) {
+			if (i > 0 && nums[i] == nums[i-1]) continue;
+			int val = - nums[i];
+			int low = i+1, high = nums.length - 1;
+			while ( low < high) {
+				if (nums[low] + nums[high] > val) {
+					high--;
+				} else if (nums[low] + nums[high] < val) {
+					low++;
+				} else {
+					res.add(Arrays.asList(nums[i],nums[low],nums[high]));
+					while (low < high && nums[low] == nums[low+1]) low++;
+					while (low < high && nums[high] == nums[high-1]) high--;
+					low++;
+					high--;
+				}
+			}
+		}
+		return res;
+	}
+}
