aa34f20eff03842dab85ce16bc7a0b72bf9ad87f
genejing <jingxinhao1992@gmail.com>
2018-02-09 23:13:16 -0800

    #2 method of Two Sum

---
+++ Assignment 2/Two sum(#2).java
@@ -0,0 +1,25 @@
+public class Solution {
+    public int[] twoSum(int[] nums, int target) {
+        int[] res = new int[2];
+        int[] array = nums.clone();
+        
+        Arrays.sort(array);
+        int lo = 0, hi = array.length - 1;
+        while (array[lo] + array[hi] != target) {
+            if (array[lo] + array[hi] > target) {
+                hi--;
+            } else {
+                lo++;
+            }
+        }
+        boolean used = false;
+        for (int i=0; i<nums.length;i++) {
+            if (nums[i] == array[lo] && !used) {
+                res[0] = i;
+                used = true;
+            }
+            if (nums[i] == array[hi]) res[1] = i;
+        }
+        return res;
+    }
+}
